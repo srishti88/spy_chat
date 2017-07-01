4ae23bbc614bd91de4323b5701b3fdb6cb18af09
srishti88 <srishti.kalia96@gmail.com>
2017-07-01 06:42:14 +0530

    Feature to read chat history added

--- message.py
+++ message.py
@@ -19,4 +19,10 @@ def decrypt_message():
 def send_message():
     friend_choice =int( select_a_friend())
     print ("your message has been successfully send to %s ")%(friends['name'][friend_choice -1])
+def read_chat_history():
+    read_for = select_a_friend()
+    for (index,chat) in enumerate(friends['chat']):
+        print ("Your message is : " + chat)
 
+
+read_chat_history()
